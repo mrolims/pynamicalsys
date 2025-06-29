90dedf7dba1d301b04e3e99315b85b66c6464a6e
Matheus Rolim Sales <matheusrolim95@gmail.com>
2025-06-29 15:32:18 +0100

    Add the Hénon-Heiles system and a 4D Rossler system

--- src/pynamicalsys/continuous_time/models.py
+++ src/pynamicalsys/continuous_time/models.py
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+from typing import Callable, Optional
+
 import numpy as np
 from numba import njit
 from numpy.typing import NDArray
-from typing import Callable, Optional
 
 
 @njit
@@ -53,6 +54,144 @@ def lorenz_jacobian(
     J[2, 0] = u[1]
     J[2, 1] = u[0]
     J[2, 2] = -beta
+
+    return J
+
+
+@njit
+def henon_heiles(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    dudt = np.zeros_like(u)
+
+    dudt[0] = u[2]  # dx / dt = px
+    dudt[1] = u[3]  # dy / dt = py
+    dudt[2] = -u[0] - 2 * u[0] * u[1]  # d(px) / dt = - x - 2xy
+    dudt[3] = -u[1] - u[0] ** 2 + u[1] ** 2  # d(py) / dt = - y - x^2 + y^2
+
+    return dudt
+
+
+@njit
+def henon_heiles_jacobian(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    neq = len(u)
+    J = np.zeros((neq, neq), dtype=np.float64)
+
+    J[0, 0] = 0
+    J[0, 1] = 0
+    J[0, 2] = 1
+    J[0, 3] = 0
+
+    J[1, 0] = 0
+    J[1, 1] = 0
+    J[1, 2] = 0
+    J[1, 3] = 1
+
+    J[2, 0] = -1 - 2 * u[1]
+    J[2, 1] = -2 * u[0]
+    J[2, 2] = 0
+    J[2, 3] = 0
+
+    J[3, 0] = -2 * u[0]
+    J[3, 1] = -1 + 2 * u[1]
+    J[3, 2] = 0
+    J[3, 3] = 0
+
+    return J
+
+
+@njit
+def rossler_system(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    a, b, c = parameters
+
+    dudt = np.zeros_like(u)
+
+    dudt[0] = -u[1] - u[2]
+    dudt[1] = u[0] + a * u[1]
+    dudt[2] = b + u[2] * (u[0] - c)
+
+    return dudt
+
+
+@njit
+def rossler_system_jacobian(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    a, b, c = parameters
+
+    neq = len(u)
+    J = np.zeros((neq, neq), dtype=np.float64)
+
+    J[0, 0] = 0
+    J[0, 1] = -1
+    J[0, 2] = -1
+
+    J[1, 0] = 1
+    J[1, 1] = a
+    J[1, 2] = 0
+
+    J[2, 0] = u[2]
+    J[2, 1] = 0
+    J[2, 2] = u[0] - c
+
+    return J
+
+
+@njit
+def rossler_system_4D(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    a, b, c, d = parameters
+    x, y, z, w = u
+    dudt = np.zeros_like(u)
+
+    dudt[0] = -(y + z)
+    dudt[1] = x + a * y + w
+    dudt[2] = b + x * z
+    dudt[3] = -c * z + d * w
+
+    return dudt
+
+
+@njit
+def rossler_system_4D_jacobian(
+    time: float, u: NDArray[np.float64], parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+
+    a, b, c, d = parameters
+    x, y, z, w = u
+
+    neq = len(u)
+    J = np.zeros((neq, neq), dtype=np.float64)
+
+    J[0, 0] = 0
+    J[0, 1] = -1
+    J[0, 2] = -1
+    J[0, 3] = 0
+
+    J[1, 0] = 1
+    J[1, 1] = a
+    J[1, 2] = 0
+    J[1, 3] = 1
+
+    J[2, 0] = z
+    J[2, 1] = 0
+    J[2, 2] = x
+    J[2, 3] = 0
+
+    J[3, 0] = 0
+    J[3, 1] = 0
+    J[3, 2] = -c
+    J[3, 3] = d
 
     return J
 
