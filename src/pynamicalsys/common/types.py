9d3c36c0ab7747e0e3f37f7b9c77f6185d459f5e
Matheus Rolim Sales <matheusrolim95@gmail.com>
2026-06-23 20:01:48 -0300

    Add type aliases for equations of motion (eom_t) and the Hessian of H (hess_H_t)

--- src/pynamicalsys/common/types.py
+++ src/pynamicalsys/common/types.py
@@ -59,6 +59,21 @@ hess_t: TypeAlias = Callable[
     NDArray[np.float64],
 ]
 
+# Equations of motion: f(q, p, parameters) -> (dq/dt, dp/dt)
+# i.e. (dH/dp, -dH/dq), supplied directly by the user (not assembled
+# from grad_T/grad_V, since H need not split that way)
+eom_t: TypeAlias = Callable[
+    [NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]],
+    tuple[NDArray[np.float64], NDArray[np.float64]],
+]
+
+# Full Hessian of H w.r.t. the combined state z = (q, p), shape (2n, 2n).
+# Block layout: [[H_qq, H_qp], [H_pq, H_pp]]
+hess_H_t: TypeAlias = Callable[
+    [NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]],
+    NDArray[np.float64],
+]
+
 symplectic_step_t: TypeAlias = Callable[
     [
         NDArray[np.float64],
