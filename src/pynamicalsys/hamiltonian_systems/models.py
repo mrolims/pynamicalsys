ef3d89b40869776189470802c9a5d4c7b2282752
Matheus Rolim Sales <matheusrolim95@gmail.com>
2026-06-24 09:50:47 -0300

    Add equations of motion and Hessian of H for the Hénon-Heiles Hamiltonian

--- src/pynamicalsys/hamiltonian_systems/models.py
+++ src/pynamicalsys/hamiltonian_systems/models.py
@@ -1,6 +1,6 @@
 # models.py
 
-# Copyright (C) 2025 Matheus Rolim Sales
+# Copyright (C) 2025-2026 Matheus Rolim Sales
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -66,3 +66,43 @@ def henon_heiles_hess_V(
     H01 = 2.0 * q0
     H11 = 1.0 - 2.0 * q1
     return np.array([[H00, H01], [H01, H11]])
+
+
+@njit
+def henon_heiles_eom(
+    q: NDArray[np.float64],
+    p: NDArray[np.float64],
+    parameters: Union[None, Sequence[float], NDArray[np.float64]] = None,
+) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+    """
+    Equations of motion for the Hénon-Heiles Hamiltonian.
+    Returns (dq/dt, dp/dt) = (grad_T(p), -grad_V(q)).
+    """
+    qdot = henon_heiles_grad_T(p, parameters)
+    pdot = -henon_heiles_grad_V(q, parameters)
+    return qdot, pdot
+
+
+@njit
+def henon_heiles_hess_H(
+    q: NDArray[np.float64],
+    p: NDArray[np.float64],
+    parameters: Union[None, Sequence[float], NDArray[np.float64]] = None,
+) -> NDArray[np.float64]:
+    """
+    Full Hessian of the Hénon-Heiles Hamiltonian H(q,p) = T(p) + V(q)
+    w.r.t. the combined state z = (q, p), shape (4, 4):
+
+        [[ Hqq,  0  ],
+         [  0,  Hpp ]]
+
+    Since H is separable here, the q-p cross block is identically zero.
+    """
+    Hqq = henon_heiles_hess_V(q, parameters)
+    Hpp = henon_heiles_hess_T(p, parameters)
+
+    n = q.shape[0]
+    H = np.zeros((2 * n, 2 * n))
+    H[:n, :n] = Hqq
+    H[n:, n:] = Hpp
+    return H
