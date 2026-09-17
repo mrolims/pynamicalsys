bae35fe788df26162ab6c6ffd7aa42d69a6d600c
Matheus Rolim Sales <matheusrolim95@gmail.com>
2026-09-17 08:00:31 -0300

    docs: fix light-mode logo by serving both logos from _static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 
-html_logo = "images/logo_dark.png"
+html_logo = "_static/logo_dark.png"
 
 mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 html_static_path = ["_static"]
